8095929b55cbc2ab7f7387869c99efafc7333aca
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2023-12-17 00:32:05 +0530

    Summary Changes

--- nlp.py
+++ nlp.py
@@ -1,15 +1,28 @@
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 
+file_path = "/home/oindrilabanerjee/Desktop/text.txt"
+
+
+def getData():
+    with open(file_path, "r") as f:
+        data = f.read()
+
+    return data
+
+
 model_name = "deepset/roberta-base-squad2"
 
-nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
-QA_input = {
-    'question': 'Which name is also used to describe the Amazon rainforest in English?',
-    'context': 'The Amazon rainforest (Portuguese: Floresta Amazônica or Amazônia; Spanish: Selva Amazónica, Amazonía or usually Amazonia; French: Forêt amazonienne; Dutch: Amazoneregenwoud), also known in English as Amazonia or the Amazon Jungle, is a moist broadleaf forest that covers most of the Amazon basin of South America. This basin encompasses 7,000,000 square kilometres (2,700,000 sq mi), of which 5,500,000 square kilometres (2,100,000 sq mi) are covered by the rainforest. This region includes territory belonging to nine nations. The majority of the forest is contained within Brazil, with 60% of the rainforest, followed by Peru with 13%, Colombia with 10%, and with minor amounts in Venezuela, Ecuador, Bolivia, Guyana, Suriname and French Guiana. States or departments in four nations contain "Amazonas" in their names. The Amazon represents over half of the planet\'s remaining rainforests, and comprises the largest and most biodiverse tract of tropical rainforest in the world, with an estimated 390 billion individual trees divided into 16,000 species.'
-}
-res = nlp(QA_input)
+# nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
+nlp = pipeline("summarization", model="facebook/bart-large-cnn")
 
-model = AutoModelForQuestionAnswering.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+# QA_input = {
+#     'question': 'How many words are there in a paragraph?',
+#     'context': getData()
+# }
 
-print(res['answer'])
+res = nlp(getData(), max_length=150, min_length=100, do_sample=False)
+#
+# model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+# tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+print(res)
